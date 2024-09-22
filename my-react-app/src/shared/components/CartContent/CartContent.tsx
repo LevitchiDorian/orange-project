a861d1ea6253fca092efbb34d53d0700fbaa347d
Levitchi Dorian <dorianucho@gmail.com>
2024-09-22 14:17:10 +0300

    Cart changes

--- my-react-app/src/shared/components/CartContent/CartContent.tsx
+++ my-react-app/src/shared/components/CartContent/CartContent.tsx
@@ -1,14 +1,28 @@
 import React from 'react';
-import "./CartContent.css";
+import './CartContent.css';
 
 const CartContent: React.FC = () => {
   return (
-    <div className='cart-content'>
-      <div className='cart-header'>
+    <div className="cart-content">
+      <div className="cart-header">
         <h2 className="cart-title">Coșul Tău</h2>
       </div>
-      <div className='cart-items'>
-        <p>Aici vor apărea produsele adăugate în coș.</p>
+
+      <div className="cart-items">
+        <div className="cart-item">
+          {/* Placeholder pentru iteme din coș */}
+          <h3>Produs 1</h3>
+          <p>Detalii produs...</p>
+        </div>
+        <div className="cart-item">
+          <h3>Produs 2</h3>
+          <p>Detalii produs...</p>
+        </div>
+      </div>
+
+      {/* Poți adăuga butoane pentru acțiuni adiționale sau total */}
+      <div className="cart-actions">
+        <button>Continuă Comanda</button>
       </div>
     </div>
   );
